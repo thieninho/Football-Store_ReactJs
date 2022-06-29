13e3d914ff79be275852dd66c7c55ef594463252
Nguyen Thanh Thien <lil.thieninho@gmail.com>
2022-06-30 01:57:29 +0700

    Update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import logo from '../../assets/images/res-logo.png'
+import logo from '../../assets/images/res-logo.png';
 import { NavLink, Link} from "react-router-dom";
+import '../../styles/header.css';
 
 const nav__link = [
   {
@@ -25,14 +26,14 @@ const nav__link = [
 const Header = () => {
   return <header className='header'>
     <Container>
-      <div className='nav__wrapper'>
+      <div className="nav__wrapper d-flex align-items-center justify-content-between">
         <div className='logo'>
           <img src= {logo} alt="logo" />
             <h5>Thieninho's Sport</h5>
         </div>
         {/* ==Menu== */}
         <div className='navigation'>
-          <div className='menu'>
+          <div className="menu d-flex align-items-center gap-5">
             {
               nav__link.map((item, index) => (
                 <NavLink to={item.path} key={index}>
@@ -42,7 +43,7 @@ const Header = () => {
           </div>
         </div>
       {/* == nav right icons ==*/}
-      <div className="nav__right">
+      <div className="nav__right d-flex align-items-center gap-3">
         <span className="cart__icon">
           <i class="ri-shopping-basket-line"></i>
           <span className="cart__badge">2</span>
